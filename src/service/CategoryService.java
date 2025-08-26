5ea9c83349ed8881ac3c157aef3bf4c061983820
kyeonghwi <kyeonghwi@gmail.com>
2025-08-26 11:13:55 +0900

    Feat: Transaction 클래스 생성

--- src/service/CategoryService.java
+++ src/service/CategoryService.java
@@ -8,4 +8,9 @@ public class CategoryService {
 	private CategoryRepository categoryRepository = new CategoryRepository();
 	private TransactionRepository transactionRepository = new TransactionRepository();
 
+	public void initDefaultCategory() {
+		if (categoryRepository.findAll().isEmpty()) {
+			categoryRepository.save(null)
+		}
+	}
 }
